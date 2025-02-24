33d8a5e6da5a388c1c6c3742d0d3f6d9ad05e11c
lalit choudhary <lalitchoudhary84596@gmail.com>
2025-02-24 12:45:29 +0530

    Backend Map Apis integrated

---
+++ backend/controllers/map.controller.js
@@ -0,0 +1,44 @@
+const mapService = require('../services/maps.service')
+const {validationResult}= require('express-validator')
+
+module.exports.getCoordinates = async(req,res,next)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors : errors.array()})
+    }
+    const { address} = req.query
+    try {
+        const cordinates = await mapService.getAddressCoordinates(address)
+        res.status(200).json(cordinates)
+    } catch (error) {
+        res.status(404).json({message : 'Coordinates not found'})
+    }
+}
+
+module.exports.getDistanceTime = async (req,res,next)=>{
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const { origin, destination } = req.query
+        const distanceTime = await mapService.getDistanceTime(origin, destination)
+        res.status(200).json(distanceTime)
+    } catch (err) {
+        res.status(500).json({ message: 'Internal server error' })
+    }
+}
+
+module.exports.getAutoCompleteSuggestions = async (req,res,next) => {
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const { input } = req.query
+        const suggestions = await mapService.getAutoCompleteSuggestions(input)
+        res.status(200).json(suggestions)
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' })
+    }
+}
---
+++ backend/services/maps.service.js
@@ -0,0 +1,67 @@
+const axios = require('axios');
+
+module.exports.getAddressCoordinates = async(address)=>{
+
+    const apiKey = process.env.GOOGLE_MAPS_API;
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
+
+    try {
+        const response = await axios.get(url);
+        if (response.data.status === 'OK') {
+            const location = response.data.results[0].geometry.location;
+            return {
+                lat: location.lat,
+                lng: location.lng
+            };
+        } else {
+            throw new Error('Unable to fetch coordinates');
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+
+}
+
+module.exports.getDistanceTime = async(origin,destination) =>{
+    if(!origin || !destination) {
+        throw new Error('origin and destination are required')
+    }
+    const apiKey = process.env.GOOGLE_MAPS_API
+    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent(origin)}&destinations=${encodeURIComponent(destination)}&key=${apiKey}`;
+    try{
+        const response = await axios.get(url)
+        if(response.data.status === 'OK'){
+            if(response.data.rows[0].elements[0].status === 'ZERO_RESULTS') {
+                throw new Error('No routes found')
+            }
+            const element = response.data.rows[0].elements[0];
+            return {
+                distance: element.distance,
+                duration: element.duration
+            };
+        } else {
+            throw new Error('Unable to fetch distance and time')
+        }
+    } catch(err){
+        throw err
+    }
+}
+
+module.exports.getAutoCompleteSuggestions = async(input) => {
+    if (!input) {
+        throw new Error('query is required');
+    }
+    const apiKey = process.env.GOOGLE_MAPS_API;
+    const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${encodeURIComponent(input)}&key=${apiKey}`;
+    try {
+        const response = await axios.get(url);
+        if (response.data.status === 'OK') {
+            return response.data.predictions;
+        } else {
+            throw new Error('Unable to fetch autocomplete suggestions');
+        }
+    } catch (error) {
+        throw error;
+    }
+}
