1216b5c4eeeae58445e18c2cbdddcfdc8ed52949
lalit choudhary <lalitchoudhary84596@gmail.com>
2025-03-05 22:08:50 +0530

    Final Commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Set the output directory for your build files
+    outDir: 'build', // or use 'dist' if you prefer the default
+  },
 })
